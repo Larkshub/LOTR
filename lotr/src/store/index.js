76b29ec8d46e825fada164d6d3f03f559353626e
Willy <acropolis_arcaica@hotmail.com>
2021-07-06 11:07:12 +0200

    Fix changes

--- lotr/src/store/index.js
+++ lotr/src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import actions from './actions';
+import state from './state';
+import mutations from './mutations';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  mutations,
+  actions,
   modules: {
-  }
-})
+  },
+});

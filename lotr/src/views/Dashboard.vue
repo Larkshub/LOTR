3a714171afaeb29a85411506e7ef0dace25b5496
Renszo Camacho <varsovia84@gmail.com>
2021-07-06 11:00:29 +0200

    Create Dashboard component

---
+++ lotr/src/views/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <ul v-for="(book,index) in bookList" :key="index">
+        </ul>
+    </main>
+</template>
+
+<script>
+import { mapActions, mapMutations, mapState } from 'vuex';
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    ...mapState(['books']),
+    ...mapMutations(['bookList']),
+  },
+  methods: {
+    ...mapActions(['getAllBooks']),
+  },
+  mounted() {
+    this.getAllBooks();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

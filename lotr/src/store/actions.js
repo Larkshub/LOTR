632013173eb33654d28d601dd38447b7b274f1b7
Willy <acropolis_arcaica@hotmail.com>
2021-07-06 11:07:38 +0200

    Fix changes in store and dashboard

--- lotr/src/store/actions.js
+++ lotr/src/store/actions.js
@@ -5,12 +5,12 @@ export default {
     const authorization = {
       headers: {
         Accept: 'application/json',
-        Authorization: 'Bearer 0St10OYxOS-IZpsuDVEn',
-      },
+        Authorization: 'Bearer 0St10OYxOS-IZpsuDVEn'
+      }
     };
     axios(process.env.VUE_APP_URL_BOOKS, authorization)
       .then((response) => {
-        commit('bookList', response.data);
+        commit('bookList', response.data.docs);
       });
-  },
+  }
 };

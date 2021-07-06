66f0133f07980bc1e95fa47e53d65d35d3d6f175
Renszo Camacho <varsovia84@gmail.com>
2021-07-06 10:35:22 +0200

    Modify VueX store - Create acton - Create state - Create mutations

---
+++ lotr/src/store/actions.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export default {
+  getAllBooks({ commit }) {
+    const authorization = {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer 0St10OYxOS-IZpsuDVEn',
+      },
+    };
+    axios(process.env.VUE_APP_URL_BOOKS, authorization)
+      .then((response) => {
+        commit('bookList', response.data);
+      });
+  },
+};
